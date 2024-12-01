50eb84fefb70b5d0bb5586a977dbf5d0d7e0e0f4
u <u.ecmaker@gmail.com>
2024-12-02 08:21:37 +0900

    feature/frontend #7 Discord channel Header.
    - Discordチャンネルのヘッダーを記述してみよう
    - html, css の理解 深まり。

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Sidebar.scss"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
 
 const Sidebar = () => {
   return (
@@ -21,6 +22,18 @@ const Sidebar = () => {
           <h3>Discord</h3>
           <ExpandMoreIcon />
         </div>
+
+        {/* sidebarChannels */}
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+        </div>
+
       </div>
     </div>
   )
