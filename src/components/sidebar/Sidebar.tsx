bfb30b5c1bac0751db7c50003db505404cd4c2a2
u <u.ecmaker@gmail.com>
2024-12-01 22:37:36 +0900

    feature/frontend #4 サイドバーのscss.
    - scssのエディタの色はテーマを変更することで出来た。

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,12 +6,19 @@ const Sidebar = () => {
     <div className="sidebar">
       {/* sidebarLeft */}
       <div className="sidebarLeft">
-
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
       </div>
 
       {/* sidebarRight */}
       <div className="sidebarRight">
-        
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+        </div>
       </div>
     </div>
   )
