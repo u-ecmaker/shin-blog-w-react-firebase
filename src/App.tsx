15cb950b92023407670c4efcd4ee83a6ed2e31a6
u <u.ecmaker@gmail.com>
2024-12-01 21:19:15 +0900

    feature/frontend #3 create sidebar.
    - サイドバーコンポーネントを作成してみよう
    - 書き始める前にどのように分けるかをイメージしてコンポーネントを分ける。

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {/* sidebar */}
+      <Sidebar />
+
+      {/* home */}
+
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Sidebar.scss"
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* sidebarLeft */}
+      <div className="sidebarLeft">
+
+      </div>
+
+      {/* sidebarRight */}
+      <div className="sidebarRight">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
