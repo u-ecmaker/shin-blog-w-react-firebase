2ee2b8dc9510a3379e51e81235dba634c3ea05f7
u <u.ecmaker@gmail.com>
2024-12-02 22:01:15 +0900

    feature/frontend #18 fix discord icon.
    - U original で scss を discord icon用に当ててみた。

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar = () => {
       {/* sidebarLeft */}
       <div className="sidebarLeft">
         <div className="serverIcon">
-          <img src="./logo192.png" alt="" />
+          <img className="discordIconImg" src="./discord-icon.png" alt="" />
         </div>
         <div className="serverIcon">
           <img src="./logo192.png" alt="" />
