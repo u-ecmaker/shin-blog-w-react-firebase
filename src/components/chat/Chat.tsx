d45bc44223681ca56ad86177eed05ccc607d486c
u <u.ecmaker@gmail.com>
2024-12-02 16:32:05 +0900

    feature/frontend #15 create chat Input form.
    - Discordチャット送信のHTMLとCSSを実装してみよう.
    - tsxの'送信'文字は邪魔なのでdisplay none にしている。Enterで送信できる。

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Chat.scss';
 import ChatHeader from './ChatHeader';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { CardGiftcard, EmojiEmotions, Gif } from '@mui/icons-material';
 
 const Chat = () => {
   return (
@@ -14,6 +16,19 @@ const Chat = () => {
 
       {/* chatInput */}
       <div className="chatInput">
+        <AddCircleOutlineIcon />
+        <form>
+          <input type="text" placeholder="#Udemyへメッセージを送信" />
+          <button type="submit" className="chatInputButton">
+            送信
+          </button>
+        </form>
+
+        <div className="chatInputIcons">
+          <CardGiftcard />
+          <Gif />
+          <EmojiEmotions />
+        </div>
       </div>
     </div>
   )
