1f901d3ce98b6b550be6f48633f6fe3b896d06cc
u <u.ecmaker@gmail.com>
2024-12-02 14:33:44 +0900

    feature/frontend #12 create Chat component.
    - Discordチャットコンポーネントを作成してみよう
    - scss: flex-grow で調整。

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Chat.scss';
+
+const Chat = () => {
+  return (
+    <div className="chat">
+      chat
+      {/* chatHeader */}
+      {/* chatMessage */}
+      {/* chatInput */}
+    </div>
+  )
+}
+
+export default Chat
