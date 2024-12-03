f734012f81d9cf247caa3bbc47d72f04e1c0a60f
u <u.ecmaker@gmail.com>
2024-12-04 07:25:56 +0900

    feature/backend2=fireStore #8 add channel creation function.
    - Discordチャンネルを追加する実装をしてみよう
    - サイドバーコンポーネントへのチャンネル作成機能の追加
    
    - 公式ドキュメントの参照 addDoc関数。
    
    - Firebase Firestoreから`db`をインポート
    - `addDoc`と`collection`関数をfirebase/firestoreからインポート
    - 新しいチャンネルを作成するための`addChannel`関数を追加
    - `AddIcon`クリック時に`addChannel`関数を発火するための`onClick`プロパティを追加

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -5,13 +5,24 @@ import SidebarChannel from './SidebarChannel';
 import MicIcon from '@mui/icons-material/Mic';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SettingsIcon from '@mui/icons-material/Settings';
-import { auth } from '../../firebase';
+import { auth, db } from '../../firebase';
 import { useAppSelector } from '../../app/hooks';
 import useCollection from "../../hooks/useCollection";
+import { addDoc, collection } from "firebase/firestore";
 
 const Sidebar = () => {
   const user = useAppSelector((state) => state.user);
   const { documents: channels } = useCollection("channels");
+
+  const addChannel = async () => {
+    let channelName: string | null = prompt("新しいチャンネルを作成します。");
+
+    if (channelName) {
+      await addDoc(collection(db, "channels"), {
+        channelName: channelName,
+      });
+    }
+  }
 
   return (
     <div className="sidebar">
@@ -39,7 +50,7 @@ const Sidebar = () => {
               <ExpandMoreIcon />
               <h4>プログラミングチャンネル</h4>
             </div>
-            <AddIcon className="sidebarAddIcon" />
+            <AddIcon className="sidebarAddIcon" onClick={() => addChannel()}/>
           </div>
 
           <div className="sidebarChannelList">
